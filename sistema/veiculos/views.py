3e2b4c23b7c9ff972e728a89aa6bce1f513f8730
estudo <adao.ribeiro@mail.uft.edu.br>
2020-11-25 16:34:27 -0300

    AULA 4 COMPLETO

--- sistema/veiculos/views.py
+++ sistema/veiculos/views.py
@@ -1,3 +1,4 @@
+'''
 from django.views.generic import View
 from django.shortcuts import render
 from veiculos.models import Veiculo
@@ -11,3 +12,17 @@ class VeiculosList(View):
             'lista_veiculos' :Veiculo.objects.all().order_by('marca')
         } 
         return render(request,'veiculos/listar.html',contexto)
+'''
+
+from django.views.generic import ListView, CreateView
+#from django.urls import reverse_lazy
+from veiculos.models import Veiculo
+#from veiculos.forms import FormularioVeiculo
+
+
+class VeiculosList(ListView):
+
+    model = Veiculo
+    context_object_name = 'lista_veiculos'
+    template_name = 'veiculos/listar.html'
+
